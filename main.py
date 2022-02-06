517fb0c2032bb94b3b4e4ec578a2ef45c9eef554
lgactna <lgonzalesna@gmail.com>
2022-02-06 00:20:24 -0800

    working but not complete threading

--- main.py
+++ main.py
@@ -41,6 +41,88 @@ def show_error_window(text, informative_text, title):
     msg.setInformativeText(informative_text)
     msg.setWindowTitle(title)
     msg.exec()
+
+class Worker(QtCore.QRunnable):
+    '''
+    Worker thread
+
+    :param args: Arguments to make available to the run code
+    :param kwargs: Keywords arguments to make available to the run code
+
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.args = args
+        self.kwargs = kwargs
+
+    @QtCore.Slot()  # QtCore.Slot
+    def run(self):
+        # https://www.pythonguis.com/tutorials/multithreading-pyside-applications-qthreadpool/
+        lib, processing_ids, tracking_ids, mp3_target_directory, data_directory, bpstat_prefix = self.args
+
+        bpstat_name = datetime.now().strftime("%Y-%m-%d %H-%M-%S (new).bpstat")
+        bpstat_path = os.path.join(data_directory, bpstat_name)
+
+        os.makedirs(data_directory, exist_ok=True)
+        os.makedirs(mp3_target_directory, exist_ok=True)
+        # bpstat generation and processing can happen at the same time
+        song_arr = []
+
+        index = 1
+        max = len(lib.songs)
+        for track_id, song in lib.songs.items():
+            print(f"processing {song.name} ({index}/{max})")
+            if track_id in processing_ids:
+                bpsynctools.copy_and_process_song(song)
+            if track_id in tracking_ids:
+                bpsynctools.add_to_bpstat(song, bpstat_prefix, bpstat_path)
+                song_arr.append(song)
+
+        # Create database with new songs
+        # TODO: Doesn't work - need to set up to be path-independent
+        #models.set_engine(data_directory)
+        models.create_db()
+        models.add_libpy_songs(song_arr)
+
+"""
+class Worker(QtCore.QRunnable):
+    '''
+    Worker thread
+    '''
+
+    @QtCore.Slot()  # QtCore.Slot
+    def run(self, pd, processing_ids, tracking_ids, mp3_target_directory, data_directory, bpstat_prefix):
+        '''
+        Your code goes in this function
+        '''
+        # this ought to go somewhere else?
+        bpstat_name = datetime.now().strftime("%Y-%m-%d %H-%M-%S (new).bpstat")
+        bpstat_path = os.path.join(data_directory, bpstat_name)
+
+        os.makedirs(data_directory, exist_ok=True)
+        os.makedirs(mp3_target_directory, exist_ok=True)
+        # bpstat generation and processing can happen at the same time
+        song_arr = []
+
+        for track_id, song in self.lib.songs.items():
+            if self.pd.wasCanceled():
+                return
+
+            if track_id in processing_ids:
+                bpsynctools.copy_and_process_song(song)
+                self.pd.setValue(self.pd.value()+1)
+            if track_id in tracking_ids:
+                bpsynctools.add_to_bpstat(song, bpstat_prefix, bpstat_path)
+                song_arr.append(song)
+
+        # Create database with new songs
+        # TODO: Doesn't work - need to set up to be path-independent
+        #models.set_engine(data_directory)
+        models.create_db()
+        models.add_libpy_songs(song_arr)
+"""
+
 class FirstTimeWindow(QtWidgets.QWidget,Ui_FirstTimeWindow):
     def __init__(self):
         super().__init__()
@@ -50,10 +132,11 @@ class FirstTimeWindow(QtWidgets.QWidget,Ui_FirstTimeWindow):
 
         self.program_path = QtCore.QDir.currentPath()
         self.lib = None  # libpytunes Library object
+        self.thread_manager = QtCore.QThreadPool()
 
         # Prepopulate fields
         self.mp3_path_lineedit.setText('/tmp')
-        self.data_path_lineedit.setText('/data/bpstat')
+        self.data_path_lineedit.setText('/data')
 
         # Signals and slots
         ## Buttons
@@ -136,6 +219,57 @@ class FirstTimeWindow(QtWidgets.QWidget,Ui_FirstTimeWindow):
             return
         
         # Oh boy
+        # Get track IDs of selected items by iterating over table widget's model data
+        data = self.table_widget.table_model.array_data
+        selected_ids_processing = []
+        selected_ids_tracking = []
+        for row in data:
+            if row[1]:  # Check for mp3 processing
+                selected_ids_processing.append(row[0])
+            if row[2]:  # Check for db tracking
+                selected_ids_tracking.append(row[0])
+
+        # Create progress bar for element processing (the longest operation)
+        # https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QProgressDialog.html#PySide2.QtWidgets.PySide2.QtWidgets.QProgressDialog.setLabel
+        # TODO: doesn't work with threading as expected
+        # TODO: tracking only doesn't work as expected - maybe just use full library?
+        
+        #self.pd = QtWidgets.QProgressDialog("Processing/copying songs.", "Cancel", 0, len(selected_ids_processing))
+        #self.pd.setWindowModality(QtCore.Qt.WindowModal)
+
+        # Start processing thread
+        # TODO: tidy up
+        worker = Worker(self.lib, selected_ids_processing, selected_ids_tracking, mp3_target_directory, data_directory, bpstat_prefix)
+        self.thread_manager.start(worker)
+
+    '''
+    def full_processing(self, processing_ids, tracking_ids, mp3_target_directory, data_directory, bpstat_prefix):
+        # this ought to go somewhere else?
+        bpstat_name = datetime.now().strftime("%Y-%m-%d %H-%M-%S (new).bpstat")
+        bpstat_path = os.path.join(data_directory, bpstat_name)
+
+        os.makedirs(data_directory, exist_ok=True)
+        os.makedirs(mp3_target_directory, exist_ok=True)
+        # bpstat generation and processing can happen at the same time
+        song_arr = []
+
+        for track_id, song in self.lib.songs.items():
+            if self.pd.wasCanceled():
+                return
+
+            if track_id in processing_ids:
+                bpsynctools.copy_and_process_song(song)
+                self.pd.setValue(self.pd.value()+1)
+            if track_id in tracking_ids:
+                bpsynctools.add_to_bpstat(song, bpstat_prefix, bpstat_path)
+                song_arr.append(song)
+
+        # Create database with new songs
+        # TODO: Doesn't work - need to set up to be path-independent
+        #models.set_engine(data_directory)
+        models.create_db()
+        models.add_libpy_songs(song_arr)
+    '''
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
