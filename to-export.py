20cfb437f58c7133551133f54eddc13e2c6d6f6a
lgactna <lgonzalesna@gmail.com>
2022-05-29 15:49:44 -0700

    add utlity export script

---
+++ to-export.py
@@ -0,0 +1,10 @@
+import models
+from bpsynctools import add_to_exportimport
+
+models.initialize_engine('data/songs.db')
+
+with models.Session() as session:
+    db_songs = session.query(models.StoredSong).all()
+
+for song in db_songs:
+    add_to_exportimport(song, "out.txt")
